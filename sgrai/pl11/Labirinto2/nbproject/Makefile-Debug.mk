4c3d14bc655b194cbc99519cbfad5856aedfa182
Luís Teixeira <teixeira13@gmail.com>
2013-11-29 21:33:34 +0000

    Projecto sgrai

---
+++ sgrai/pl11/Labirinto2/nbproject/Makefile-Debug.mk
@@ -0,0 +1,90 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/1472/grafos.o \
+	${OBJECTDIR}/_ext/1472/lapr5.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=`pkg-config --libs gl` `pkg-config --libs glu`  
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/labirinto2
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/labirinto2: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/labirinto2 ${OBJECTFILES} ${LDLIBSOPTIONS} -lglut
+
+${OBJECTDIR}/_ext/1472/grafos.o: ../grafos.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1472
+	${RM} $@.d
+	$(COMPILE.cc) -g `pkg-config --cflags gl` `pkg-config --cflags glu`   -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1472/grafos.o ../grafos.cpp
+
+${OBJECTDIR}/_ext/1472/lapr5.o: ../lapr5.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1472
+	${RM} $@.d
+	$(COMPILE.cc) -g `pkg-config --cflags gl` `pkg-config --cflags glu`   -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1472/lapr5.o ../lapr5.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/labirinto2
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
